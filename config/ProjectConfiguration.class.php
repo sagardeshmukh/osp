225f52a354681d7e73bc328730cfb340d199f4d2
sagardeshmukh <sagar.deshmukh_2626@rediffmail.com>
2015-05-31 14:08:46 +0530

    update Project config file

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -2,7 +2,7 @@
 if(!is_dir('D:/xampp'))
 	require_once '/var/chroot/home/content/39/11374039/html/dev/lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
 else
-	require_once 'd:/xampp/htdocs/yozoadev/sf_app/lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
+	require_once 'd:/xampp/htdocs/osp/lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
